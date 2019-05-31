2a9a16ca86efa39ee8d13ee2b68f5ee953d40084
Star Sonwabo Zuma <sonwabilestarzuma@outlook.com>
2019-05-31 01:07:27 -0700

    Add project files.

---
+++ MyHubStudents/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyHubStudents.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
